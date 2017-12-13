b3b1c0d702cfba2c1951ec952ab6127f659415ba
lilelife <1663595737@qq.com>
2017-12-13 23:24:49 +0800

    添加注解

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -2,13 +2,17 @@ package controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
- 
+
+@org.springframework.stereotype.Controller
 public class IndexController implements Controller {
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ModelAndView mav = new ModelAndView("index.jsp");
+	
+	@RequestMapping("/index")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("index");
         mav.addObject("message", "Hello Spring MVC");
         return mav;
     }
